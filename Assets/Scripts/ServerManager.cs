9a326483ac865ec23c627b29c958fbedf73be8b2
Ali Eren <ali_erenbatman@hotmail.com>
2023-03-29 22:30:15 +0300

    Sahneyi düzenleme ve multiplayer oynamayı ayarladım

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Servere bağlanıldı");
+        Debug.Log("Lobiye bağlanılıyor");
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("Lobiye bağlanıldı");
+        Debug.Log("Odaya bağlanılıyor");
+        PhotonNetwork.JoinOrCreateRoom("Odaismi", new RoomOptions {MaxPlayers=2, IsOpen=true, IsVisible=true}, TypedLobby.Default); ;
+    }
+   
+    public override void OnJoinedRoom() 
+    {
+        base .OnJoinedRoom();
+        Debug.Log("Odaya bağlanıldı");
+        Debug.Log("Karakter oluşturuluyor...");
+        PhotonNetwork.Instantiate("Kemal", new Vector3(-10,5,-5), Quaternion.identity, 0, null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
